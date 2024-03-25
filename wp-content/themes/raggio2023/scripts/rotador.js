1f92052e77bf5650ec6aed9c1ea617628627542e
alitamazaira <118550381+alitamazaira@users.noreply.github.com>
2024-03-25 14:36:51 +0000

    avances

--- wp-content/themes/raggio2023/scripts/rotador.js
+++ wp-content/themes/raggio2023/scripts/rotador.js
@@ -1,9 +1,11 @@
 $(document).ready(function () {
   $('.slides').slick({
     dots: true,
+    autoplay: true,
+    useAutoplayToggleButton:false,
     adaptiveHeight: true
   });
-	$('#hero').slick({
+	$('#hero .rotador').slick({
 		dots: true,
     autoplay: true,
     useAutoplayToggleButton:false
