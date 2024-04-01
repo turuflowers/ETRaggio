f288f27d6b5077a37835b2cc823a49fc4350e551
Nicolás Flores <turuflowers@gmail.com>
2024-04-01 20:33:37 +0000

    base index

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php 
+get_header(); 
+?>
+
+<div id="contenido" class="contenedor">
+    <?php
+    if (have_posts()) :
+        while (have_posts()) : the_post();
+            ?>
+            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                <header class="entry-header">
+                    <h2 class="entry-title"><?php the_title(); ?></h2>
+                </header>
+
+                <div class="entry-content">
+                    <?php the_content(); ?>
+                </div>
+
+                <footer class="entry-footer">
+                    <?php
+                    if (comments_open() || get_comments_number()) :
+                        comments_template();
+                    endif;
+                    ?>
+                </footer>
+            </article>
+        <?php
+        endwhile;
+    else :
+        ?>
+        <p><?php esc_html_e('No se encontraron entradas.', 'tu_tema'); ?></p>
+    <?php
+    endif;
+    ?>
+</div>
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
--- wp-content/themes/raggio2023/functions.php
+++ wp-content/themes/raggio2023/functions.php
@@ -4,8 +4,8 @@
 function registrar_menus() {
     register_nav_menus(
         array(
-            'menu_principal' => __('Menú Principal', 'tu_tema'),
-            'menu_secundario' => __('Menú Secundario', 'tu_tema'),
+            'menu_principal' => __('Menú Header', 'etraggio'),
+            'menu_pie' => __('Menú Footer', 'etraggio'),
         )
     );
 }
@@ -15,8 +15,8 @@ add_action('init', 'registrar_menus');
 function agregar_clases_a_menu($classes, $item, $args) {
     if ($args->theme_location == 'menu_principal') {
         $classes[] = 'clase-menu-principal'; // Puedes cambiar 'clase-menu-principal' por la clase que desees
-    } elseif ($args->theme_location == 'menu_secundario') {
-        $classes[] = 'clase-menu-secundario'; // Puedes cambiar 'clase-menu-secundario' por la clase que desees
+    } elseif ($args->theme_location == 'menu_pie') {
+        $classes[] = 'clase-menu-pie'; // Puedes cambiar 'clase-menu-pie' por la clase que desees
     }
     return $classes;
 }
