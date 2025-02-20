01bb06a8c1a7981ab975c2fe67b0d12c2619287b
alitamazaira <118550381+alitamazaira@users.noreply.github.com>
2025-02-20 09:43:37 +0000

    Ajuste de Js para esconder noticias en el home

--- wp-content/themes/raggio2023/scripts/masnoticias.js
+++ wp-content/themes/raggio2023/scripts/masnoticias.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
     // Ocultar todas menos las primeras 6 al cargar la página
-    $('#noticiasHome .cajaNoticias:gt(6)').addClass('hidenoticia'); 
+    $('#noticiasHome .cajaNoticias:not(.hide):gt(5)').addClass('hidenoticia'); 
 
     // Mostrar 6 noticias más al hacer clic en el botón
     $('#masnoticias').on('click', function () {
-        $('#noticiasHome .cajaNoticias.hidenoticia').slice(0, 6).removeClass('hidenoticia');
+        $('#noticiasHome .cajaNoticias.hidenoticia').not('.hide').slice(0, 6).removeClass('hidenoticia');
 
         // Si no quedan más noticias ocultas, ocultar el botón
-        if ($('#noticiasHome .cajaNoticias.hidenoticia').length === 0) {
+        if ($('#noticiasHome .cajaNoticias.hidenoticia').not('.hide').length === 0) {
             $(this).addClass('hide');
         }
     });
